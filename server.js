002fd2d292347567f026a88f85380eeb38f1251f
koiclone <cheryljcruz1@gmail.com>
2022-04-21 16:04:00 -0500

    create routes to get html files

--- server.js
+++ server.js
@@ -20,7 +20,17 @@ app.get("/api/notes", (req, res) => {
 });
 
 // get html files
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
 
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
 
 app.post('/api/notes', (req, res) => {
     console.log(req.body);
